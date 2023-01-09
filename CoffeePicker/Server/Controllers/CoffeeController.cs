d5e207aa43509416bfe13b378f87bf4b9ff7bb5a
Serkan ISIK <dehnas79@gmail.com>
2023-01-09 14:07:59 +0300

    Add GetDetails with Bootstrap

--- CoffeePicker/Server/Controllers/CoffeeController.cs
+++ CoffeePicker/Server/Controllers/CoffeeController.cs
@@ -18,6 +18,11 @@ namespace CoffeePicker.Server.Controllers
             new(3,"Cappuccino","Cappuccino is a latte made with more foam than steamed milk, often with a sprinkle of cocoa powder or cinnamon on top. Sometimes you can find variations that use cream instead of milk or ones that throw in flavor shot, as well.", new string[]{ "Espresso","Steamed Milk","Foam" }),
             new(4,"Americano","With a similar flavor to black coffee, the americano consists of an espresso shot diluted in hot water.", new string[]{ "Coffee","Hot Water" }),
             new(5,"Espresso","An espresso shot can be served solo or used as the foundation of most coffee drinks, like lattes and macchiatos.", new string[]{ "1oz Espresso" }),
+            new(6, "Doppio", "A double shot of espresso, the doppio is perfect for putting extra pep in your step.", new string[] { "1oz Espresso" }),
+            new(7, "Cortado", "Like yin and yang, a cortado is the perfect balance of espresso and warm steamed milk. The milk is used to cut back on the espresso’s acidity.", new string[] { "1oz Espresso", "1oz Steamed Milk" }),
+            new(8, "Red Eye", "Named after those pesky midnight flights, a red eye can cure any tiresome morning. A full cup of hot coffee with an espresso shot mixed in, this will definitely get your heart racing.", new string[] { "Coffee", "Espresso" }),
+            new(9, "Galão", "Originating in Portugal, this hot coffee drink is closely related to the latte and cappuccino. Only difference is it contains about twice as much foamed milk, making it a lighter drink compared to the other two.", new string[] { "Espresso", "Foamed milk" }),
+            new(10, "Lungo", "A lungo is a long-pull espresso. The longer the pull, the more caffeine there is and the more ounces you can enjoy.", new string[] { "Long pulled espresso" }),
         };
 
         [HttpGet("{id}")]
